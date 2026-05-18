6b65419c4919e32ffc7acfd6fb9c7ee379c89a18
ShoxjahonToxtaboyev <nodirbekmatyakubov050@gmail.com>
2026-05-18 12:53:52 +0500

    edd

---
+++ 18-may 2026 o'zgaruvchilar/main.js
@@ -0,0 +1,51 @@
+//^ console.log() konsol oynaga natijani chiqaradi
+
+// console.log("Hello world!")
+
+//^ o'zgaruvchilar
+
+// let buMeningIsmim = "Shohjahon"
+// // console.log(buMeningIsmim)
+
+// // // buMeningIsmim = "Jasurbek"
+// // // // console.log(buMeningIsmim)
+
+
+// // // // // const yoshim = 14
+// // // // // // console.log(yoshim)
+
+// // // // // // // yoshim = 15
+
+// // // // // // console.log(yoshim)
+
+
+//^ Operatorlar
+
+let son1 = 125
+let son2 = 57
+
+let yigindi = son1 + son2
+console.log(yigindi)
+
+let ayirma = son1 - son2
+console.log(ayirma)
+
+let bolish = son1 / son2
+console.log(bolish)
+
+let qoldiq = son1 % son2
+console.log(qoldiq)
+
+let daraja = son1 ** son2
+console.log(daraja)
+
+let kopaytrish = son1 * son2
+console.log(kopaytrish)
+
+// Topshiriq
+
+let son3 = 1234
+let son4 = 48
+let son5 = 5
+let son6 = 600
+
