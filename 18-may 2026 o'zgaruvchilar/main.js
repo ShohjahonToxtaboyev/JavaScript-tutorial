d7f1dcc334c15013d170269f705c610ef8e8df3c
ShoxjahonToxtaboyev <nodirbekmatyakubov050@gmail.com>
2026-05-19 21:04:46 +0500

    edd

--- 18-may 2026 o'zgaruvchilar/main.js
+++ 18-may 2026 o'zgaruvchilar/main.js
@@ -21,26 +21,26 @@
 
 //^ Operatorlar
 
-let son1 = 125
-let son2 = 57
+// let son1 = 125
+// let son2 = 57
 
-let yigindi = son1 + son2
-console.log(yigindi)
+// let yigindi = son1 + son2
+// console.log(yigindi)
 
-let ayirma = son1 - son2
-console.log(ayirma)
+// let ayirma = son1 - son2
+// console.log(ayirma)
 
-let bolish = son1 / son2
-console.log(bolish)
+// let bolish = son1 / son2
+// console.log(bolish)
 
-let qoldiq = son1 % son2
-console.log(qoldiq)
+// let qoldiq = son1 % son2
+// console.log(qoldiq)
 
-let daraja = son1 ** son2
-console.log(daraja)
+// let daraja = son1 ** son2
+// console.log(daraja)
 
-let kopaytrish = son1 * son2
-console.log(kopaytrish)
+// let kopaytrish = son1 * son2
+// console.log(kopaytrish)
 
 // Topshiriq
 
@@ -49,3 +49,5 @@ let son4 = 48
 let son5 = 5
 let son6 = 600
 
+let = yigndi = son3 + son4 + son5 + son6
+console.log(yigndi) 
