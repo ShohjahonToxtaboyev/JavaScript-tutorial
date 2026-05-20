2da6e88bfdda5b16f4cc08e4391aaac523c0af22
ShoxjahonToxtaboyev <nodirbekmatyakubov050@gmail.com>
2026-05-20 11:33:05 +0500

    edd

--- 18-may 2026 o'zgaruvchilar/main.js
+++ 18-may 2026 o'zgaruvchilar/main.js
@@ -14,12 +14,12 @@ console.log(buMeningIsmim)
 const yoshim = 14
 console.log(yoshim)
 
-yoshim = 15
+yoshim1 = 15
 
-console.log(yoshim)
+console.log(yoshim1)
 
 
-^ Operatorlar
+
 
 let son1 = 125
 let son2 = 57
@@ -42,12 +42,29 @@ console.log(daraja)
 let kopaytrish = son1 * son2
 console.log(kopaytrish)
 
-Topshiriq
+
 
 let son3 = 1234
 let son4 = 48
 let son5 = 5
 let son6 = 600
 
-let = yigndi = son3 + son4 + son5 + son6
-console.log(yigndi) 
+let = yigndi1 = son3 + son4 + son5 + son6
+console.log(yigndi1) 
+
+let ayirma1 = son3 - son4 
+console.log(ayirma1)
+
+let bolish1 = son5 / son6
+console.log(bolish1)
+
+let qoldiq1 = son3 % son4
+console.log(qoldiq1)
+
+let daraja1 = son5 ** son6
+console.log(daraja1)
+
+let kopaytrish1 = son3 * son4
+console.log(kopaytrish1)
+
+
