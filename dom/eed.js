7086b0153fe878d63aba1cab25036f3ad0752bc5
ShoxjahonToxtaboyev <nodirbekmatyakubov050@gmail.com>
2026-08-10 04:21:14 -0300

    edd

--- dom/eed.js
+++ dom/eed.js
@@ -1,3 +1,34 @@
 const box = document.getElementById('box')
-console.log(box)
-box.style.backgroundColor = "blue"
+const box1 = document.getElementById('box1')
+const box2 = document.getElementById('box2')
+const box3 = document.getElementById('box3')
+const box4 = document.getElementById('box4')
+const box5 = document.getElementById('box5')
+const box6 = document.getElementById('box6')
+
+
+
+
+
+
+
+
+
+
+
+
+
+box1.style.borderRadius = "20px"
+box1.style.borderRadius="20px"
+box1.style.border="1px solid black"
+box1.style.width="120px"
+box1.style.height="90px"
+box1.style.alignItems="center"
+
+
+
+box.style.borderRadius = "20px"
+box.style.borderRadius="20px"
+box.style.border="1px solid black"
+box.style.width="1400px"
+box.style.height="500px"
