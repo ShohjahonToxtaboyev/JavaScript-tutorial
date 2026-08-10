bf29d43a10925450d6f9606700596e7eff602dea
ShoxjahonToxtaboyev <nodirbekmatyakubov050@gmail.com>
2026-08-10 05:04:07 -0300

    edd

---
+++ 10-Avgust 2026/awudhsef.js
@@ -0,0 +1,21 @@
+const div = document.getElementById('div')
+const h1 = document.getElementById('h1')
+const p = document.getElementById('p')
+  
+div.style.borderRadius = "20px"
+div.style.borderRadius="20px"
+div.style.border="1px solid black"
+div.style.width="1420px"
+div.style.height="1000px"
+
+h1.style.borderRadius = "20px"
+h1.style.borderRadius="20px"
+h1.style.border="1px solid black"
+h1.style.width="120px"
+h1.style.height="90px"
+
+p.style.borderRadius = "20px"
+p.style.borderRadius="20px"
+p.style.border="1px solid black"
+p.style.width="120px"
+p.style.height="90px"
