83c0b85363512c0bc849401a0c06908f94dc7b25
ShoxjahonToxtaboyev <nodirbekmatyakubov050@gmail.com>
2026-08-21 16:13:07 -0300

    edd

--- topshriq/topshrq.js
+++ topshriq/topshrq.js
@@ -1,13 +1,11 @@
 const sunToggle = document.getElementById('sunToggle');
 
 sunToggle.addEventListener('click', () => {
-  // Klasni qo'shadi yoki o'chiradi
   document.body.classList.toggle('dark-mode');
   
-  // Rejimga qarab ikonkani almashtiradi
   if (document.body.classList.contains('dark-mode')) {
-    sunToggle.textContent = '🌙'; // Qorong'u bo'lsa oy
+    sunToggle.textContent = '🌙'; 
   } else {
-    sunToggle.textContent = '☀️'; // Yorug' bo'lsa quyosh
+    sunToggle.textContent = '☀️'; 
   }
 });
